ee33888793c5148156d2e00491850be8517f3a9d
Abderrahmane <abdrr97@gmail.com>
2021-09-21 16:39:30 +0100

    making Home Page and installing Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+// pages
+import Home from './pages/Home'
+// auth
 
 const App = () => {
   return (
     <>
-      <h1>React E-commerce</h1>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          {/* auth */}
+          <Route path='/login' component={Home} />
+          <Route path='/signup' component={Home} />
+          <Route path='/forgot-password' component={Home} />
+
+          {/* Produits */}
+          <Route path='/products' component={Home} />
+          <Route path='/products/:id' component={Home} />
+          <Route path='/checkout' component={Home} />
+          <Route path='/user/create/category' component={Home} />
+          <Route path='/user/create/product' component={Home} />
+          <Route path='/search' component={Home} />
+
+          <Route path='*'>
+            <h1>Page Not Found</h1>
+          </Route>
+        </Switch>
+      </Router>
     </>
   )
 }
